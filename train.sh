165fed7e5b3b0cbdde9751abcd7d1a5095d7478c
jradig <jean.radig@uni-heidelberg.de>
2024-06-25 07:50:25 +0000

    Instructions on how to run scDiffusion are in the jupyter notebook how_to_run.ipynb

--- train.sh
+++ train.sh
@@ -1,15 +1,15 @@
 cd VAE
 echo "Training Autoencoder, this might take a long time"
-CUDA_VISIBLE_DEVICES=0 python VAE_train.py --data_dir '/data1/lep/Workspace/guided-diffusion/data/tabula_muris/all.h5ad' --num_genes 18996 --save_dir '../checkpoint/AE/my_VAE' --max_steps 200000
+CUDA_VISIBLE_DEVICES=0 python VAE_train.py --data_dir 'path/to/anndata.h5ad' --num_genes 18996 --save_dir 'path/to/saved_VAE_model' --max_steps 200000
 echo "Training Autoencoder done"
 
 cd ..
 echo "Training diffusion backbone"
-CUDA_VISIBLE_DEVICES=0 python cell_train.py --data_dir '/data1/lep/Workspace/guided-diffusion/data/tabula_muris/all.h5ad' --num_genes 18996 --ae_dir 'checkpoint/AE/my_VAE/model_seed=0_step=150000.pt' \
-    --model_name 'my_diffusion' --lr_anneal_steps 800000
+CUDA_VISIBLE_DEVICES=0 python cell_train.py --data_dir 'path/to/anndata.h5ad'  --vae_path 'path/to/saved_VAE_model/VAE_checkpoint.pt' \
+    --save_dir 'path/to/saved_diffusion_model' --model_name 'my_diffusion' --save_interval 20000
 echo "Training diffusion backbone done"
 
 echo "Training classifier"
-CUDA_VISIBLE_DEVICES=0 python classifier_train.py --data_dir '/data1/lep/Workspace/guided-diffusion/data/tabula_muris/all.h5ad' --classifier_dir "checkpoint/classifier/my_classifier" \
-    --iterations 400000 --num_genes 18996 --ae_dir 'checkpoint/AE/my_VAE/model_seed=0_step=150000.pt'
+CUDA_VISIBLE_DEVICES=0 python classifier_train.py --data_dir 'path/to/anndata.h5ad' --model_path "path/to/saved_classifier_model" \
+    --iterations 40000 --vae_path 'path/to/saved_VAE_model/VAE_checkpoint.pt'
 echo "Training classifier, done"
